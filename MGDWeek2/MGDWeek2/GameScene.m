2f86d2bc6f01d95eef28a90ee5ee88e08b390842
Brenna Pavlinchak <littlemeg27@fullsail.edu>
2015-06-15 19:15:55 -0400

    Project2 sunday nights

--- MGDWeek2/MGDWeek2/GameScene.m
+++ MGDWeek2/MGDWeek2/GameScene.m
@@ -43,62 +43,79 @@ static const uint32_t edgeCategory  = 2; // 00000000000000000000000000000010
         CGPoint location1 = [touch locationInNode:self];
         CGPoint location2 = [touch locationInNode:self];
         CGPoint location3 = [touch locationInNode:self];
+        CGPoint location4 = [touch locationInNode:self];
         
         SKAction *playSFX1 = [SKAction playSoundFileNamed:@"blip.caf" waitForCompletion:NO];
         [self runAction:playSFX1];
         
         //Create a new sprite node from an image
-        SKSpriteNode* card1 = [SKSpriteNode spriteNodeWithImageNamed:@"MickeyCard1.png"];
+        SKSpriteNode* card1 = [SKSpriteNode spriteNodeWithImageNamed:@"disneyCard1.png"];
         CGPoint cardPoint1 = CGPointMake(160,200);
         card1.position = cardPoint1;
         
-        SKSpriteNode* card2 = [SKSpriteNode spriteNodeWithImageNamed:@"MickeyCard2"];
+        SKSpriteNode* card2 = [SKSpriteNode spriteNodeWithImageNamed:@"disneyCard2.png"];
         CGPoint cardPoint2 = CGPointMake(240,460);
         card2.position = cardPoint2;
         
-        SKSpriteNode* card3 = [SKSpriteNode spriteNodeWithImageNamed:@"MickeyCard3"];
+        SKSpriteNode* card3 = [SKSpriteNode spriteNodeWithImageNamed:@"disneyCard3.png"];
         CGPoint cardPoint3 = CGPointMake(240,460);
-        card2.position = cardPoint3;
+        card3.position = cardPoint3;
+        
+        SKSpriteNode* card4 = [SKSpriteNode spriteNodeWithImageNamed:@"disneyCard4.png"];
+        CGPoint cardPoint4 = CGPointMake(160,200);
+        card4.position = cardPoint4;
         
         //Give the sprite nodes some gravity
         card1.physicsBody = [SKPhysicsBody bodyWithTexture:card1.texture size:card1.texture.size];
         card2.physicsBody = [SKPhysicsBody bodyWithTexture:card2.texture size:card2.texture.size];
         card3.physicsBody = [SKPhysicsBody bodyWithTexture:card3.texture size:card3.texture.size];
+        card4.physicsBody = [SKPhysicsBody bodyWithTexture:card4.texture size:card4.texture.size];
         
         card1.physicsBody.friction = 0;
         card1.physicsBody.linearDamping = 0;
         card1.physicsBody.restitution = 1.0f;
         card1.position = location1;
         card1.physicsBody.categoryBitMask = cardCategory;
-        card1.physicsBody.contactTestBitMask = edgeCategory | cardCategory;
+        card1.physicsBody.contactTestBitMask = edgeCategory;
         
         card2.physicsBody.friction = 0;
         card2.physicsBody.linearDamping = 0;
         card2.physicsBody.restitution = 1.0f;
         card2.position = location2;
         card2.physicsBody.categoryBitMask = cardCategory;
-        card1.physicsBody.contactTestBitMask = edgeCategory;
+        card2.physicsBody.contactTestBitMask = edgeCategory;
         
         card3.physicsBody.friction = 0;
         card3.physicsBody.linearDamping = 0;
         card3.physicsBody.restitution = 1.0f;
         card3.position = location3;
         card3.physicsBody.categoryBitMask = cardCategory;
-        card1.physicsBody.contactTestBitMask = edgeCategory;
+        card3.physicsBody.contactTestBitMask = edgeCategory | cardCategory;
+        
+        card4.physicsBody.friction = 0;
+        card4.physicsBody.linearDamping = 0;
+        card4.physicsBody.restitution = 1.0f;
+        card4.position = location4;
+        card4.physicsBody.categoryBitMask = cardCategory;
+        card4.physicsBody.contactTestBitMask = edgeCategory | cardCategory;
+        
         
         [self addChild:card1];
         [self addChild:card2];
         [self addChild:card3];
+        [self addChild:card4];
         
         //Create the vector
         CGVector cardVector1 = CGVectorMake(15, 15);
         CGVector cardVector2 = CGVectorMake(15, 15);
         CGVector cardVector3 = CGVectorMake(15, 15);
+        CGVector cardVector4 = CGVectorMake(15, 15);
         
         //Apply the vector
         [card1.physicsBody applyImpulse:cardVector1];
         [card2.physicsBody applyImpulse:cardVector2];
         [card3.physicsBody applyImpulse:cardVector3];
+        [card4.physicsBody applyImpulse:cardVector4];
     }
     
 }
