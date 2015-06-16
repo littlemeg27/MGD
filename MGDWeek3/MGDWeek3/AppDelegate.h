49a73faf74f5d46610b3d7b65ba9e47fc8d80ec1
Brenna Pavlinchak <littlemeg27@fullsail.edu>
2015-06-15 23:27:14 -0400

    Starting Week3

---
+++ MGDWeek3/MGDWeek3/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MGDWeek3
+//
+//  Created by Brenna Pavlinchak on 6/15/15.
+//  Copyright (c) 2015 Brenna Pavlinchak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
